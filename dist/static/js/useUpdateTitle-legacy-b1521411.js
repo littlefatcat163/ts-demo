39dcef89c80b185a8744c1f3e067669512f47815
theMxb <674160430@qq.com>
2023-04-26 21:27:51 +0800

    build(dist): remove unused

--- dist/static/js/useUpdateTitle-legacy-b1521411.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-ef54a768.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.f}],execute:function(){e("u",((e="",n="")=>{const u=t(),c=()=>{document.title=n||e,u.updateTitle(e)};Vue.onMounted(c),Vue.onActivated(c)}))}}}));
